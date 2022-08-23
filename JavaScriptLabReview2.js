cd0207bfd28b4b86933d4d64d126b453a000f0ad
BrandonELin <theb.lin01@gmail.com>
2022-08-23 14:02:41 -0400

    Where is Waldo Answered

--- JavaScriptLabReview2.js
+++ JavaScriptLabReview2.js
@@ -78,8 +78,16 @@ console.log(favMovies.indexOf('Volver'))
 //const defines a constant reference, not a constant array
 
 /////////////////////////////////
-//
+//Where is Waldo
 /////////////////////////////////
+const whereIsWaldo = [["Timmy", "Frank"], "Eggbert",
+                     ["Lucinda", "Jacc", "Neff", "Snoop"],
+                     ["Petunia", ["Baked Goods", "Waldo"]]];
+whereIsWaldo.splice(1,1)
+whereIsWaldo[1].splice(2,1,"No One")
+
+console.log(whereIsWaldo)
+console.log(whereIsWaldo[2][1][1])
 
 /////////////////////////////////
 //
